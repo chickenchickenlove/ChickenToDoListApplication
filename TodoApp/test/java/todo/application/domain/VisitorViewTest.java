adca9268d9c80b3821147c57b0d4e76d58596858
ChickenchickenLove <ojt90902@naver.com>
2022-02-16 22:59:11 +0900

    Add files via upload
    [신규 기능] 통계 기능 추가
    1. 조회수, 회원 가입 시, 글 작성 수, 로그인 방문자 수 확인을 위한 통계용 엔티티 도메인 테스트 코드 추가.

---
+++ TodoApp/test/java/todo/application/domain/VisitorViewTest.java
@@ -0,0 +1,84 @@
+package todo.application.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class VisitorViewTest {
+
+    @Test
+    void createMethodTest() throws Exception{
+        //given + when
+        VisitorView visitorView = VisitorView.createVisitorView();
+
+        //then
+        assertThat(visitorView.getView()).isEqualTo(0L);
+        assertThat(visitorView.getLoginView()).isEqualTo(0L);
+        assertThat(visitorView.getDate()).isEqualTo(LocalDate.now());
+        assertThat(visitorView.getUserJoin()).isEqualTo(0L);
+        assertThat(visitorView.getWriteCreatedNumber()).isEqualTo(0L);
+
+    }
+
+    @Test
+    void unitTest1() throws Exception{
+        //given
+        VisitorView visitorView = VisitorView.createVisitorView();
+
+        //when
+        visitorView.addView(1L);
+
+        //then
+        assertThat(visitorView.getView()).isEqualTo(1L);
+    }
+
+
+    @Test
+    void unitTest2() throws Exception{
+        //given
+        VisitorView visitorView = VisitorView.createVisitorView();
+
+        //when
+        visitorView.addLoginView(1L);
+
+        //then
+        assertThat(visitorView.getLoginView()).isEqualTo(1L);
+    }
+
+
+    @Test
+    void unitTest3() throws Exception{
+        //given
+        VisitorView visitorView = VisitorView.createVisitorView();
+
+        //when
+        visitorView.addUserJoin(1L);
+
+        //then
+        assertThat(visitorView.getUserJoin()).isEqualTo(1L);
+    }
+
+
+    @Test
+    void unitTest4() throws Exception{
+        //given
+        VisitorView visitorView = VisitorView.createVisitorView();
+
+        //when
+        visitorView.addWriteCreatedNumber(1L);
+
+        //then
+        assertThat(visitorView.getWriteCreatedNumber()).isEqualTo(1L);
+    }
+
+
+
+
+
+
+}
