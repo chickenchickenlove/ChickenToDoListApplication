e8c857d1a01eb6ee2f7aef58221856bbe4db21c6
ChickenchickenLove <ojt90902@naver.com>
2022-02-17 23:38:35 +0900

    Add files via upload
    [코드 분리] MemberService → MemberArticleService
    1. MemberService에 있던 MemberArticle 엔티티 코드를 모두 MemberArticleService로 이동.

--- TodoApp/main/java/todo/application/service/MemberArticleService.java
+++ TodoApp/main/java/todo/application/service/MemberArticleService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import todo.application.domain.MemberArticle;
 import todo.application.repository.MemberArticleRepository;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -18,11 +20,21 @@ public class MemberArticleService {
     private final MemberArticleRepository memberArticleRepository;
 
 
+    // 단건 조회 //
+
     public MemberArticle findMemberArticleByMemberIdAndArticleId(Long memberId, Long articleId) {
         return memberArticleRepository.findMemberArticleByMemberIdArticleId(memberId, articleId);
     }
 
 
+    // 리스트 조회 //
+
+    public List<MemberArticle> findArticleListByMemberId(Long memberId) {
+        return memberArticleRepository.findMemberArticleByMemberId(memberId);
+    }
+
+
+    // 슬라이싱 조회 //
     public Slice<MemberArticle> findPagingArticleByMemberIdNotCompleted(Long memberId, Pageable pageable) {
         return memberArticleRepository.findSliceArticleByMemberIdNotCompleted(memberId, pageable);
     }
