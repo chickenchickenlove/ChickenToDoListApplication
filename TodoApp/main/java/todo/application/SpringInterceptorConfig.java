3a26c497790c22be4b548a8567173b4cadccf503
ChickenchickenLove <ojt90902@naver.com>
2022-02-17 23:42:54 +0900

    Add files via upload
    [신규 기능] Admin 페이지
    1. Admin 페이지용 보안 인터셉터 등록

--- TodoApp/main/java/todo/application/SpringInterceptorConfig.java
+++ TodoApp/main/java/todo/application/SpringInterceptorConfig.java
@@ -1,12 +1,15 @@
 package todo.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import todo.application.controller.formatter.LocalDateTimeToStringConverter;
-import todo.application.controller.formatter.StringToDateFormatter;
+import todo.application.controller.formatter.LocalDateToStringConverter;
+import todo.application.controller.formatter.StringToLocalDateFormatter;
+import todo.application.controller.interceptor.AdminLoginInterceptor;
 import todo.application.controller.interceptor.LoginInterceptor;
+import todo.application.repository.MemberArticleRepository;
 
 @Component
 public class SpringInterceptorConfig implements WebMvcConfigurer {
@@ -15,14 +18,19 @@ public class SpringInterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .order(1)
-                .addPathPatterns("/article/**")
-                .excludePathPatterns("/css/**", "/*.ico", "/error","/bootstrap/**");
+                .addPathPatterns("/article/**", "/share", "/admin/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/bootstrap/**");
+
+        registry.addInterceptor(new AdminLoginInterceptor())
+                .order(2)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/bootstrap/**");
     }
 
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-//        registry.addConverter(new StringToDateFormatter());
-//        registry.addConverter(new LocalDateTimeToStringConverter());
+        registry.addConverter(new StringToLocalDateFormatter());
+        registry.addConverter(new LocalDateToStringConverter());
     }
 }
